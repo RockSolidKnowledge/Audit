e7a30d54cc1adbd53b3c110fc7587aa171dc504e
Croisetière Alain <croisetierea@grics.ca>
2020-03-09 14:22:34 -0400

    fix resource actor type of consent denied event

--- Src/Adapters/ConsentGrantedEventAdapter.cs
+++ Src/Adapters/ConsentGrantedEventAdapter.cs
@@ -15,7 +15,7 @@ namespace RSK.IdentityServer4.AuditEventSink.Adapters
             _evt = evt;
         }
 
-        public ResourceActor Actor => new ResourceActor(ResourceActor.UserSubjectType, _evt.ClientId, _evt.ClientId);
+        public ResourceActor Actor => new ResourceActor(ResourceActor.MachineSubjectType, _evt.ClientId, _evt.ClientId);
         public string Action => _evt.Name;
         public AuditableResource Resource => new AuditableResource("User", _evt.SubjectId);
         public FormattedString Description => _evt.ToString().SafeForFormatted();
