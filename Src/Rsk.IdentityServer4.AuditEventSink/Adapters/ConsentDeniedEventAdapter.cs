fa6e36a3b7c3cb5c96ee0f5397bb4f20ad0b51f0
Scott Brady <scott@scottbrady91.com>
2021-02-23 15:43:33 +0000

    Fixed consent granted and denied events to represent user performing action against the client application

--- Src/Rsk.IdentityServer4.AuditEventSink/Adapters/ConsentDeniedEventAdapter.cs
+++ Src/Rsk.IdentityServer4.AuditEventSink/Adapters/ConsentDeniedEventAdapter.cs
@@ -13,9 +13,9 @@ namespace RSK.IdentityServer4.AuditEventSink.Adapters
             this.evt = evt ?? throw new ArgumentNullException(nameof(evt));
         }
 
-        public ResourceActor Actor => new ResourceActor(ResourceActor.MachineSubjectType, evt.ClientId, evt.ClientId);
+        public ResourceActor Actor => new ResourceActor(ResourceActor.UserSubjectType, evt.SubjectId, evt.SubjectId);
         public string Action => evt.Name;
-        public AuditableResource Resource => new AuditableResource("User", evt.SubjectId);
+        public AuditableResource Resource => new AuditableResource("Client", evt.ClientId);
         public FormattedString Description => evt.ToString().SafeForFormatted();
     }
 }
